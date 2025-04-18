4263c68e63a08a0edf2577394b36d00994a6d970
nnemiha <125380868+nnemiha@users.noreply.github.com>
2025-04-18 19:24:03 +0200

    Update settings.py

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/dist'),
 ]

2bef8f5c493bbda7ca24b2eeac410a6fcc62d050
nnemiha <suskikovsuslik9@gmail.com>
2025-04-21 17:15:22 +0200

    lava

--- buld.sh
+++ buld.sh
@@ -9,14 +9,14 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input --clear
 
 # Сборка фронтенда
-cd frontend
-npm install
-npm run build
-cd ..
+#cd frontend
+#npm install
+#npm run build
+#cd ..
 
 # Копирование собранных файлов фронтенда
-mkdir -p staticfiles
-cp -r frontend/dist/* staticfiles/
+# mkdir -p staticfiles
+# cp -r frontend/dist/* staticfiles/
 
 # Сборка бэкенда
 python manage.py collectstatic --no-input

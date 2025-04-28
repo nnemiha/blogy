4f0c8d9cb8eeef3744dd5e6dc14e54bd6b6166a4
nnemiha <suskikovsuslik9@gmail.com>
2025-04-28 18:06:17 +0200

    в

--- buld.sh
+++ buld.sh
@@ -2,8 +2,25 @@
 # exit on error
 set -o errexit
 
+# Установка Python зависимостей
 pip install -r requirements.txt
 
+# Очистка старых статических файлов
+python manage.py collectstatic --no-input --clear
+
+# Сборка фронтенда
+cd frontend
+npm install
+npm run build
+cd ..
+
+# Копирование собранных файлов фронтенда
+mkdir -p staticfiles
+cp -r frontend/dist/* staticfiles/
+
+# Сборка бэкенда
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Создание суперпользователя
 python create_superuser.py 

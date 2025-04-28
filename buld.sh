ceaf274d150992d165c189e16498bf03004e603f
nnemiha <suskikovsuslik9@gmail.com>
2025-04-28 17:29:30 +0200

    d

--- buld.sh
+++ buld.sh
@@ -2,27 +2,8 @@
 # exit on error
 set -o errexit
 
-# Установка Python зависимостей
 pip install -r requirements.txt
 
-# Очистка старых статических файлов
-python manage.py collectstatic --no-input --clear
-
-# Сборка фронтенда
-#cd frontend
-#npm install
-#npm run build
-#cd ..
-
-# Копирование собранных файлов фронтенда
-# mkdir -p staticfiles
-# cp -r frontend/dist/* staticfiles/
-
-# Сборка бэкенда
-
-python manage.py migrate
-
-# Создание суперпользователя
-python createsuperuser.py 
 python manage.py collectstatic --no-input
-echo staticfiles собраны
+python manage.py migrate
+python create_superuser.py 

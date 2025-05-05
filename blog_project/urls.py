cc8e1e315a7f1ae55cbb51dffbfa5b589764da25
nnemiha <suskikovsuslik9@gmail.com>
2025-05-05 17:26:22 +0200

    d

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -1,15 +1,24 @@
+# blog_project/urls.py
+
 from django.contrib import admin
 from django.urls import path, include
+
+# Добавьте эти два импорта:
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # Сначала свои кастомные views (например, signup)
+    # Сначала свои кастомные views (signup и т.п.)
     path('accounts/', include('accounts.urls')),
-
-    # Потом стандартные Django auth (login/logout/password reset и т.д.)
+    # Потом стандартные auth
     path('accounts/', include('django.contrib.auth.urls')),
 
     path('', include('blog.urls')),
 ]
+
+# Только при DEBUG=TRUE: отдать статику из STATIC_ROOT
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
